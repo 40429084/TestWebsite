54ad38b462ec6898748cce9789b5b9e5f41f00aa
40429084 <78416395+40429084@users.noreply.github.com>
2021-03-23 13:10:03 +0000

    functions added

--- code.js
+++ code.js
@@ -1,4 +1,22 @@
-function caveFunction(){
-	knight = new Audio("JungleSound.wav");
-	knight.play();
+function jungleFunction(){
+	jungle = new Audio("JungleSound.wav");
+	jungle.play();
 	}
+	
+function caveFunction(){
+	cave = new Audio("caveSound.wav");
+	cave.play();
+	}
+	
+function winnerFunction(){
+	winner = new Audio("Winner.mp3");
+	winner.play();
+	}
+	
+function deathFunction(){
+	death = new Audio("GameOver.mp3");
+	death.play();
+	}
+
+function homeFunction(){
+	home = new audio("homeSound.wav")
