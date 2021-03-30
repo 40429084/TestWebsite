44f9c0f6224dcf20da68dbe8de7d027351e9269c
40429084 <78416395+40429084@users.noreply.github.com>
2021-03-30 12:08:36 +0100

    added new images and fixed external javascript

--- code.js
+++ code.js
@@ -1,23 +1,24 @@
-function caveFunction(){
-	cave = new Audio("caveSound.wav");
-	cave.play();
-	}
-
 function jungleFunction(){
 	jungle = new Audio("JungleSound.wav");
 	jungle.play();
-	}
-	
+	};
+
+function caveFunction() {
+	cave = new Audio("caveSound.wav");
+	cave.play();
+	};
 	
 function winnerFunction(){
 	winner = new Audio("Winner.mp3");
 	winner.play();
-	}
+	};
 	
 function deathFunction(){
 	death = new Audio("GameOver.mp3");
 	death.play();
-	}
+	};
 
 function homeFunction(){
 	home = new audio("homeSound.wav")
+	home.play();
+	};
