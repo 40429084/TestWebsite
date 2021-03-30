e7c9e6fde319b9d13bfb1b24c18855d0004748a1
40429084 <78416395+40429084@users.noreply.github.com>
2021-03-30 13:52:57 +0100

    changes

---
+++ Mage/mage.js
@@ -0,0 +1,32 @@
+function jungleFunction(){
+	jungle = new Audio("../Sounds/JungleSound.wav");
+	jungle.play();
+	};
+
+function caveFunction() {
+	cave = new Audio("../Sounds/caveSound.wav");
+	cave.play();
+	};
+	
+function winnerFunction(){
+	winner = new Audio("Sounds/Winner.mp3");
+	winner.play();
+	};
+	
+function deathFunction(){
+	death = new Audio("Sounds/GameOver.mp3");
+	death.play();
+	};
+
+function homeFunction(){
+	home = new audio("Sounds/homeSound.wav");
+	home.play();
+	};
+	
+function fightFunction(){
+	alert("Fighting the beast is dangerous and may have consequences");
+	};
+
+function fleeFunction(){
+	alert("running is always an option if you dont think you can defeat the beast");
+	};
