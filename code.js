87c9ab241762b06c4f4801a7cfe95218dbf1e2ee
40429084 <78416395+40429084@users.noreply.github.com>
2021-03-30 11:25:27 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,13 @@
+function caveFunction(){
+	cave = new Audio("caveSound.wav");
+	cave.play();
+	}
+
 function jungleFunction(){
 	jungle = new Audio("JungleSound.wav");
 	jungle.play();
 	}
 	
-function caveFunction(){
-	cave = new Audio("caveSound.wav");
-	cave.play();
-	}
 	
 function winnerFunction(){
 	winner = new Audio("Winner.mp3");
